f68221d5fc63bd8acb3bf79e4fb70e7b5619fa28
Doug McBride <doug@sneakyfrog.com>
2009-07-05 22:08:02 -0700

    Fix top50 toggle

--- irc/riffbot.rb
+++ irc/riffbot.rb
@@ -86,9 +86,9 @@ class Riffbot < Chatbot
             hash
           end
 
-          @logger.debug changed_riffs.pretty_inspect
+          @logger.info "changed_riffs = #{changed_riffs.pretty_inspect}"
 
-          changed_riffs.each{|s| s.delete :iriff} unless @options[:top_50]
+          changed_riffs.each{|s| s.last.delete :iriff} unless @options[:top_50]
 
           send_report event, changed_riffs
           @riff_stats = new_stats
